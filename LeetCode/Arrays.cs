bd2eef096a44573415a48d19c00ec909899926ae
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-08-29 20:41:16 -0300

    [Add] Binary Search Problem

---
+++ LeetCode/Arrays.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.LeetCode
+{
+    public class Arrays
+    {
+        /*------------------------ 704. BINARY SEARCH ------------------------*/
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
