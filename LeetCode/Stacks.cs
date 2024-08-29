ea95ed3169414b55632bbfe841e25e2e295f116f
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-08-29 20:36:55 -0300

    [Add] To Lower Case Problem

--- LeetCode/Stacks.cs
+++ LeetCode/Stacks.cs
@@ -6,32 +6,43 @@ using System.Threading.Tasks;
 
 namespace AdaPrepTech.LeetCode
 {
-    /* ------------------- 20 VALID PARENTHESES --------------------------------*/
-    /*
-     * public bool IsValid(string s) {
-        Stack<char> stackS = new Stack<char>();
+    public class Stacks
+    {
+        /* ------------------- 20 VALID PARENTHESES --------------------------------*/
+        public bool IsValid(string s)
+        {
+            Stack<char> stackS = new Stack<char>();
 
-        foreach(char c in s){
-            if(c == '(' || c == '{' || c == '['){
-                stackS.Push(c); 
-            } else {
-                if (stackS.Count == 0) {
-                    return false; 
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stackS.Push(c);
                 }
-                var r = stackS.Pop(); 
-                if(r == '(' && c != ')'){
-                    return false; 
-                }                
-                if(r == '{' && c != '}'){
-                    return false; 
+                else
+                {
+                    if (stackS.Count == 0)
+                    {
+                        return false;
+                    }
+                    var r = stackS.Pop();
+                    if (r == '(' && c != ')')
+                    {
+                        return false;
+                    }
+                    if (r == '{' && c != '}')
+                    {
+                        return false;
+                    }
+                    if (r == '[' && c != ']')
+                    {
+                        return false;
+                    }
                 }
-                if(r == '[' && c != ']'){
-                    return false; 
-                }                
             }
-        }
 
-        return stackS.Count == 0; 
-    }
-     */
+            return stackS.Count == 0;
+        }
+    }   
+
 }
---
+++ LeetCode/Strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.LeetCode
+{
+    public class Strings
+    {
+        /*------------------ 709. TO LOWER CASE ----------------------- */
+        public string ToLowerCase(string s)
+        {
+            char[] newWord = new char[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char current = s[i];
+
+                if (current >= 'A' && current <= 'Z')
+                {
+                    newWord[i] = (char)(current + 32);
+                }
+                else
+                {
+                    newWord[i] = current;
+                }
+            }
+
+            return new string(newWord);
+
+        }
+    }
+}
