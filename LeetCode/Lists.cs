a5e83c672a48599e8d66f799bceeea06976107a0
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-08-29 19:53:51 -0300

    [Update] Add LeetCode responses

---
+++ LeetCode/Lists.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.LeetCode
+{
+    /* ----------------------- 1290 - CONVERT BINARY NUMBER IN A LINKED LIST TO INTEGER --------------------- */
+    /*   
+     *  public int GetDecimalValue(ListNode head) {        
+        ListNode current = head; 
+        int num = 0;
+
+        while(current != null){
+            num = num * 2 + current.val;
+            current = current.next;
+        }
+
+        return num; 
+    }*/
+}
---
+++ LeetCode/Stacks.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.LeetCode
+{
+    /* ------------------- 20 VALID PARENTHESES --------------------------------*/
+    /*
+     * public bool IsValid(string s) {
+        Stack<char> stackS = new Stack<char>();
+
+        foreach(char c in s){
+            if(c == '(' || c == '{' || c == '['){
+                stackS.Push(c); 
+            } else {
+                if (stackS.Count == 0) {
+                    return false; 
+                }
+                var r = stackS.Pop(); 
+                if(r == '(' && c != ')'){
+                    return false; 
+                }                
+                if(r == '{' && c != '}'){
+                    return false; 
+                }
+                if(r == '[' && c != ']'){
+                    return false; 
+                }                
+            }
+        }
+
+        return stackS.Count == 0; 
+    }
+     */
+}
