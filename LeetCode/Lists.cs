43cef574a6d94792d034061dbace5b2d8f546949
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-07 19:50:19 -0300

    [Update] LetCode Challanges

--- LeetCode/Lists.cs
+++ LeetCode/Lists.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdaPrepTech.DataStrutures.Lists;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,16 +8,49 @@ using System.Threading.Tasks;
 namespace AdaPrepTech.LeetCode
 {
     /* ----------------------- 1290 - CONVERT BINARY NUMBER IN A LINKED LIST TO INTEGER --------------------- */
-    /*   
-     *  public int GetDecimalValue(ListNode head) {        
-        ListNode current = head; 
-        int num = 0;
+    public class Lists
+    {
 
-        while(current != null){
-            num = num * 2 + current.val;
-            current = current.next;
+        //Time: O(N)
+        //Space: O(1)
+        public int GetDecimalValue(Node head)
+        {
+            Node current = head;
+            int num = 0;
+
+            while (current != null)
+            {
+                num = num * 2 + current.Data;
+                current = current.Next;
+            }
+
+            return num;
         }
 
-        return num; 
-    }*/
+        /*-----------------------  217 Contains Duplicate - LeetCode* ----------------------- */
+
+        //Time:O(N^2)
+        //Space:O(N)
+        public bool ContainsDuplicate(int[] nums)
+        {
+            List<int> list = new List<int>();
+
+            foreach (int num in nums) //O(n)
+            {
+                if (list.Contains(num)) //O(n)
+                {
+                    return true;
+                }
+                else
+                {
+                    list.Add(num);
+                }
+            }
+
+            return false;
+        }
+    }
+
+
+
 }
