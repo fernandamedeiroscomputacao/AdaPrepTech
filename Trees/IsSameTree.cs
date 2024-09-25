c88ef34726e9610ff55cb960f6ac2ad9dd4353fb
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-25 19:49:54 -0300

    [Update] IsSameTree

---
+++ Trees/IsSameTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Trees
+{
+    public class IsSameTree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        /************ 100 - Is Same Tree ***************/
+        public bool IsSameTreeProblem(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTreeProblem(p.left, q.left) && IsSameTreeProblem(p.right, q.right);
+        }
+    }
+}
