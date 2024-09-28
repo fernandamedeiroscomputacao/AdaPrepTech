7efedb78b9d5a715deb1246fe3bed889a1521c18
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-28 18:09:48 -0300

    [Add] Mock 27/9 question

---
+++ Matrix/SetZeroesProblem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Matrix
+{
+    public class SetZeroesProblem
+    {
+
+        /**** 73. Set Matrix Zeroes *********/
+        public void SetZeroes(int[][] matrix)
+        {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            List<int> indexM = new List<int>();
+            List<int> indexN = new List<int>();
+
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        indexM.Add(i);
+                        indexN.Add(j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (indexM.Contains(i))
+                    {
+                        matrix[i][j] = 0;
+                    }
+                    if (indexN.Contains(j))
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
