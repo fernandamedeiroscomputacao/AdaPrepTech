fe4f445e982dd79e3d406d5635844113e034c406
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-29 20:44:08 -0300

    [Add] Happy Number

---
+++ Math/IsHappyProblem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Math
+{
+    public class IsHappyProblem
+    {
+        /***********202 Happy Number *************/
+        public bool IsHappy(int n)
+        {
+            HashSet<int> squares = new HashSet<int>();
+            var sum = n;
+
+            while (sum != 1)
+            {
+                sum = SumSquare(sum);
+                if (squares.Contains(sum))
+                {
+                    return false;
+                }
+
+                squares.Add(sum);
+            }
+
+            return true;
+        }
+
+        private int SumSquare(int n)
+        {
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int dig = n % 10;
+                sum += dig * dig;
+                n /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
