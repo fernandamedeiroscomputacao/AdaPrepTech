ea7943369ed2bb86ccd843314c2225dee5aee4a7
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-29 21:52:06 -0300

    [Add] SortedSquaresProblem

---
+++ Order/SortedSquaresProblem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Order
+{
+    public class SortedSquaresProblem
+    {
+        /******** 977 Squares of a Sorted Array **********/
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            PriorityQueue<int, int> squares = new PriorityQueue<int, int>();
+
+            foreach (int num in nums)
+            {
+                var square = num * num;
+                squares.Enqueue(square, square);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = squares.Dequeue();
+            }
+
+            return result;
+
+        }
+    }
+}
