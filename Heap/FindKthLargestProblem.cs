4b890af71fdf076b91125c90705a1c7cc931dd55
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-29 21:04:18 -0300

    [Add] FindKthLargest

---
+++ Heap/FindKthLargestProblem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Heap
+{
+    public class FindKthLargestProblem
+    {
+        /********215 FindKthLargest *********/
+        public int FindKthLargest(int[] nums, int k)
+        {
+            PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                minHeap.Enqueue(nums[i], nums[i]);
+                if (minHeap.Count > k)
+                {
+                    minHeap.Dequeue();
+                }
+            }
+
+            return minHeap.Peek();
+        }
+    }
+}
