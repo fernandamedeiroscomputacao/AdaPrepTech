79d37e1e453b0f37f40cce052fd7a8eac988bf45
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-09-29 21:37:18 -0300

    [Add] SeachRotate

---
+++ BinarySearch/SearchRotateArrayProblem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.BinarySearch
+{
+    public class SearchRotateArrayProblem
+    {
+        /*********** 33 search-in-rotated-sorted-array *******/
+        public int Search(int[] nums, int target)
+        {
+            int index = -1;
+
+            if (nums == null || nums.Length == 0)
+            {
+                return index;
+            }
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (nums[left] <= nums[mid])
+                {
+                    if (nums[left] <= target && target < nums[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] < target && target <= nums[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+}
