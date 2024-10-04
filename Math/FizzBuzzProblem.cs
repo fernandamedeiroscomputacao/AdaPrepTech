8e7093ddfd4c4bab5ac9c2282231c8ab4cab452f
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-10-04 18:47:46 -0300

    [Add] FizzBuzzProblem

---
+++ Math/FizzBuzzProblem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Math
+{
+    public class FizzBuzzProblem
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            List<string> result = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+            return result;
+        }
+    }
+}
