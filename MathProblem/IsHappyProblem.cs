2d1d5134f2a699d9a8a8a1ce01be42e3dc3ac68b
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-10-13 21:12:42 -0300

    [Add] DiameterOfBinaryTree

--- MathProblem/IsHappyProblem.cs
+++ MathProblem/IsHappyProblem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdaPrepTech.Math
+namespace AdaPrepTech.MathProblem
 {
     public class IsHappyProblem
     {
---
+++ Trees/DiameterOfBinaryTreeProblem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Trees
+{
+    public class DiameterOfBinaryTreeProblem
+    {
+        //543        
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public class Solution
+        {
+            private int diameter;
+
+            public int DiameterOfBinaryTree(TreeNode root)
+            {
+                diameter = 0;
+                DiameterBTAux(root);
+                return diameter;
+            }
+
+            public int DiameterBTAux(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return 0;
+                }
+
+                int leftDepth = DiameterBTAux(root.left);
+                int rightDepth = DiameterBTAux(root.right);
+
+                diameter = Math.Max(diameter, leftDepth + rightDepth);
+
+                return Math.Max(leftDepth, rightDepth) + 1;
+
+            }
+        }
+    }
+}
