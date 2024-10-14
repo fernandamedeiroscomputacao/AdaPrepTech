af281fec69bb7760687d92054cd9c876f2b44229
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-10-13 21:23:44 -0300

    [Add] MaxProfitProblem

---
+++ Arrays/MaxProfitProblem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Arrays
+{
+    public class MaxProfitProblem
+    {
+        //121
+        public int MaxProfit(int[] prices)
+        {
+            int minPrice = prices[0];
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+                else
+                {
+                    int profit = prices[i] - minPrice;
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
