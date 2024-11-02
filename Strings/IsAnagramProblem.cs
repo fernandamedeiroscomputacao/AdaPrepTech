8a4da38399078d169f5862f51feccd512dff2851
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-11-02 19:52:59 -0300

    [Add]IsAnagram

---
+++ Strings/IsAnagramProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Strings
+{
+    public class IsAnagramProblem
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            char[] sArray = s.ToCharArray();
+            Array.Sort(sArray);
+
+            char[] tArray = t.ToCharArray();
+            Array.Sort(tArray);
+
+            return sArray.SequenceEqual(tArray);
+        }
+    }
+}
