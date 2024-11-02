9c444b86d6879c280b4fe85888198b681e9442ce
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-11-02 19:57:49 -0300

    [add] CombinationSum

---
+++ Backtracking/CombinationSumProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.Backtracking
+{
+    public class CombinationSumProblem
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            Array.Sort(candidates);
+            Backtrack(candidates, target, 0, new List<int>(), result);
+            return result;
+        }
+
+        private void Backtrack(int[] candidates, int target, int start, List<int> currentCombination, IList<IList<int>> result)
+        {
+            if (target == 0)
+            {
+                result.Add(new List<int>(currentCombination));
+                return;
+            }
+
+            if (target < 0)
+            {
+                return;
+            }
+
+            for (int i = start; i < candidates.Length; i++)
+            {
+                currentCombination.Add(candidates[i]);
+                Backtrack(candidates, target - candidates[i], i, currentCombination, result);
+                currentCombination.RemoveAt(currentCombination.Count - 1);
+            }
+        }
+    }
+}
