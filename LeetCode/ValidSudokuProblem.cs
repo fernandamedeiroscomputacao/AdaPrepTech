909aeb8b5581c1d85b2db408f71e2e8358ca3924
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2024-11-16 10:11:15 -0300

    [Add] Valid Sudoku

---
+++ LeetCode/ValidSudokuProblem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaPrepTech.LeetCode
+{
+    public class ValidSudokuProblem
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            HashSet<char>[] rows = new HashSet<char>[9];
+            HashSet<char>[] cols = new HashSet<char>[9];
+            HashSet<char>[] boxes = new HashSet<char>[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                rows[i] = new HashSet<char>();
+                cols[i] = new HashSet<char>();
+                boxes[i] = new HashSet<char>();
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    char value = board[i][j];
+
+                    if (value == '.')
+                    {
+                        continue;
+                    }
+
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+                    if (rows[i].Contains(value) || cols[j].Contains(value) || boxes[boxIndex].Contains(value))
+                    {
+                        return false;
+                    }
+
+                    rows[i].Add(value);
+                    cols[j].Add(value);
+                    boxes[boxIndex].Add(value);
+                }
+            }
+
+            return true;
+
+        }
+    }
+}
