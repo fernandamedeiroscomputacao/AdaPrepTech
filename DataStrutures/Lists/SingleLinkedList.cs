b19a3ab44d51c81474dc196e5a474e9c3e6e85f3
Fernanda Medeiros <fernandamedeiroscomputacao@gmail.com>
2025-01-07 21:08:58 -0300

    [Add] MergeTwoLists (linked)

--- DataStrutures/Lists/SingleLinkedList.cs
+++ DataStrutures/Lists/SingleLinkedList.cs
@@ -383,5 +383,22 @@ namespace AdaPrepTech.DataStrutures.Lists
 
             return prev;
         }
+
+        public Node MergeTwoLists(Node list1, Node list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            if (list1.Data < list2.Data)
+            {
+                list1.Next = MergeTwoLists(list1.Next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.Next = MergeTwoLists(list1, list2.Next);
+                return list2;
+            }
+        }
     }
 }
